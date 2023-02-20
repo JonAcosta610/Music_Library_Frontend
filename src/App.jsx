3c3d1db90a679990c1a334fba6865512fa82cf49
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-02-20 16:53:29 -0500

    created use effect function to retrieve a get response from backend

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  useEffect(() => {
+    getAllSongs()
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data);
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
