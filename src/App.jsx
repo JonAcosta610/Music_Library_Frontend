77b1c51bca38b9bd35b271bd681589611f22d7d8
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-02-26 13:51:58 -0500

    rendered all song data to dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,35 @@ import axios from 'axios';
 
 function App() {
 
+  const [songs, setSongs] = useState([])
+  // console.log(songs)
   useEffect(() => {
     getAllSongs()
   }, []);
 
-  async function getAllSongs(){
+  async function getAllSongs() {
     const response = await axios.get('http://127.0.0.1:8000/api/music/');
-    console.log(response.data);
+    const allSongs = response.data
+    setSongs(allSongs)
   }
 
+  async function postSong(){
+    const response = await axios.post('http://127.0.0.1:8000/api/music/');
+  }
+  console.log(songs)
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Music Library</h1>
+      {songs.map((song) =>(
+        <div>
+          <p>{song?.title}</p>
+          <p>{song?.artist}</p>
+          <p>{song?.album}</p>
+          <p>{song?.genre}</p>
+          <p>{song?.release_date}</p>
+          <hr/>
+        </div>
+        ))}
     </div>
   );
 }
